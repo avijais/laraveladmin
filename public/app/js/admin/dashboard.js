57febd5f74a7bbc5a7fdf0d89376555e373f29f7
avijais <abhi.jais@yahoo.co.in>
2017-02-27 00:19:41 +0530

    comment console.log and used global variable for making http request

--- public/app/js/admin/dashboard.js
+++ public/app/js/admin/dashboard.js
@@ -1,3 +1,15 @@
+// var BASEURL = 'http://localhost/laraveladmin/public/';
+
+// $.ajaxPrefilter(function( options ) {
+//     // if ( !options.beforeSend) {
+//         options.beforeSend = function (xhr) { 
+//             // xhr.setRequestHeader('CUSTOM-HEADER-KEY', 'CUSTOM-HEADER-VALUE');
+//             xhr.setRequestHeader('Authorization','Bearer ' + response.data.api_token);
+//         }
+//     // }
+// });
+
+// console.log(response.data.api_token);
 $(document).ready(function () {
     if(window.location.href.indexOf("dashboard") > -1) {
        $('body').removeClass('login-page')
@@ -17,7 +29,7 @@ $('body').on('click', '#user_mgt', function(){
 function usermanagement(){
 
     $.ajax({
-        url: "http://localhost/laraveladmin/public/admin/usersList",
+        url: BASEURL + "admin/usersList",
         type: 'GET',
         statusCode: {
             404: function () {
@@ -28,22 +40,26 @@ function usermanagement(){
             }
         },
         beforeSend: function (xhr) {
+            // xhr.setRequestHeader("Accept","application/vvv.website+json;version=1");
+            // xhr.setRequestHeader("Authorization","Token token=\"FuHCLyY46\"");   
+            // xhr.setRequestHeader('Authorization','Bearer ' + response.data.api_token);
             // showLoader();
         },
         success: function (request, status, errorThrown) {
             $(".content").html('');
             $(".content").html(request);
-            console.log(status)
+            // console.log(status)
             // hideLoader();
         },
         error: function (request, status, errorThrown) {
             if (request.status == 401) {
                 //Redirect to login page 
-                window.location.href = FMconfig.BASEURL + "/admin?msg=" + encodeURIComponent(request.responseJSON.message);
+                // window.location.href = FMconfig.BASEURL + "/admin?msg=" + encodeURIComponent(request.responseJSON.message);
+                window.location.href = BASEURL + "/admin";
             }
             if (request.status == 403) {
                 //Redirect to login page
-                window.location.href = FMconfig.BASEURL + "/admin/dashboard";
+                window.location.href = BASEURL + "/admin";
             }
         }
     });
