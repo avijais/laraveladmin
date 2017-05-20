8bca2054c5416627b6182ff80b4d25ac112aa276
avijais <abhi.jais@yahoo.co.in>
2017-05-20 17:39:59 +0530

    deleted all data

--- public/app/js/admin/user_list.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#example').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": "http://localhost/laraveladmin/public/admin/fetchUsers"
-    });
-} );
