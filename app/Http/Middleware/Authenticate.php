0d64c3b8fd2ce8590843b52d1175be8818559838
avijais <abhi.jais@yahoo.co.in>
2017-05-20 17:33:11 +0530

    redirect after logout

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -17,11 +17,20 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        /**
+         *Auth::guard($guard)->guest();
+         *print false if not guest
+         */
+        // $u = Auth::guard('api')->user();
+        // print_r($u);
+        // die();
         if (Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('login');
+                //write url where you want to redirect when click also on logout
+                return redirect()->guest('admin');
+                // return redirect()->guest('login');
             }
         }
 
